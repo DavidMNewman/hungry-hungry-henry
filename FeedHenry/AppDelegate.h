4080dd7bf5851f39aae7761274697abe8c65cff3
David Newman <david.newman@bluefletch.com>
2016-02-12 14:26:41 -0500

    Initial Commit

---
+++ FeedHenry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeedHenry
+//
+//  Created by David Newman on 2/12/16.
+//  Copyright © 2016 bluefletch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FeedHenry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeedHenry
+//
+//  Created by David Newman on 2/12/16.
+//  Copyright © 2016 bluefletch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
